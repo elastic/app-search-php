3ea279c6d68a4ddae3faab9adeabad93bc9162e0
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-01-25 17:15:37 +0100

    Add php-cs-fixer to the generated code.

--- resources/dev/generate-endpoints-code.sh
+++ resources/dev/generate-endpoints-code.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 set -e
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_DIR=`cd $SCRIPT_DIR/../..; pwd`
-IMAGE=swiftype/swiftype-php-openapi-generator
+GENERATOR_IMAGE=swiftype/swiftype-php-openapi-generator
 
-cd $SCRIPT_DIR && docker build --target runner -t $IMAGE swiftype-php-codegen
+FIXER_IMAGE=herloct/php-cs-fixer
+FIXER_RULES="@Symfony,-phpdoc_no_package,-phpdoc_annotation_without_dot"
 
-docker run --rm -v ${ROOT_DIR}:/local $IMAGE generate -g swiftype-php \
-                                                      -i /local/resources/api/api-spec.yml \
-                                                      -o /local/ \
-                                                      -c /local/resources/api/config.json
+cd ${SCRIPT_DIR} && docker build --target runner -t ${GENERATOR_IMAGE} swiftype-php-codegen
 
-#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-#ROOT_DIR=`cd $SCRIPT_DIR/../..; pwd`
-#IMAGE=openapitools/openapi-generator-cli:v3.3.4
+docker run --rm -v ${ROOT_DIR}:/local ${GENERATOR_IMAGE} generate -g swiftype-php \
+                                                                  -i /local/resources/api/api-spec.yml \
+                                                                  -o /local/ \
+                                                                  -c /local/resources/api/config.json
 
-#docker run --rm -v ${ROOT_DIR}:/local $IMAGE generate -g php \
-#                                                       -i /local/resources/api/api-spec.yml \
-#                                                       -o /local/ \
-#                                                       -c /local/resources/api/config.json \
-#                                                       -t /local/resources/dev/codegen-php-template/
-#
-#
-# docker run --rm -v ${ROOT_DIR}:/local $IMAGE cat /local/composer.json
+
+docker run --rm -v ${ROOT_DIR}:/project ${FIXER_IMAGE} fix --config=.php_cs.dist \
+                                                           --rules=${FIXER_RULES}
