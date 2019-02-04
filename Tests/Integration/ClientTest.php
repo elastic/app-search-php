49431df45ca7f3f0c86faf79faba131f3fbc5c9e
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-02-04 10:01:16 +0100

    Add client integration tests (error handling and auth).

---
+++ Tests/Integration/ClientTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * This file is part of the Swiftype App Search PHP Client package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Swiftype\AppSearch\Tests\Integration;
+
+use Swiftype\AppSearch\ClientBuilder;
+use Swiftype\AppSearch\Client;
+
+/**
+ * Testing client instantiaton and error handling.
+ *
+ * @package Swiftype\AppSearch\Test\Integration
+ *
+ * @author  Aurélien FOUCRET <aurelien.foucret@elastic.co>
+ */
+class ClientApiTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * Test instantiation of a client through the client builder.
+     */
+    public function testClientBuilder()
+    {
+        $client = ClientBuilder::create($_ENV['ST_API_ENDPOINT'], $_ENV['ST_API_KEY'])->build();
+        $this->assertInstanceOf(Client::class, $client);
+    }
+
+    /**
+     * Test exception throwned when providing an invalid API endpoint.
+     *
+     * @param string API Endpoint.
+     * @param string Class of the exception that should be raised.
+     *
+     * @testWith ["http://localhost:5000", "\\Swiftype\\AppSearch\\Exception\\CouldNotConnectToHostException"]
+     *           ["http://foo.bar:5000", "\\Swiftype\\AppSearch\\Exception\\CouldNotResolveHostException"]
+     *           ["_foo_", "\\Swiftype\\AppSearch\\Exception\\UnexpectedValueException"]
+     */
+    public function testConnectionErrors($apiEndpoint, $exceptionClass)
+    {
+        $this->expectException($exceptionClass);
+        $client = ClientBuilder::create($apiEndpoint, $_ENV['ST_API_KEY'])->build();
+        $client->listEngines();
+    }
+
+    /**
+     * Test an Authentication exception is thrown when providing an in valid API Key.
+     *
+     * @expectedException \Swiftype\AppSearch\Exception\AuthenticationException
+     */
+    public function testAuthenticationError()
+    {
+        $client = ClientBuilder::create($_ENV['ST_API_ENDPOINT'], "not-an-api-key")->build();
+        $client->listEngines();
+    }
+}
