b225e3ef2e78b6c4c1b7d415fd83a76d2b0a4383
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-03-07 14:28:49 +0100

    Query suggestion implementation.

---
+++ Endpoint/QuerySuggestion.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of the Swiftype PHP Client package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Swiftype\AppSearch\Endpoint;
+
+/**
+ * Implementation of the  endpoint.
+ *
+ * @package Swiftype\AppSearch\Endpoint
+ */
+class QuerySuggestion extends \Swiftype\Endpoint\AbstractEndpoint
+{
+    // phpcs:disable
+    /**
+     * @var string
+     */
+    protected $method = 'POST';
+
+    /**
+     * @var string
+     */
+    protected $uri = '/engines/{engine_name}/query_suggestion';
+
+    protected $routeParams = ['engine_name'];
+
+    protected $paramWhitelist = ['query', 'types.documents.fields', 'size'];
+    // phpcs:enable
+}
---
+++ Tests/Integration/QuerySuggestionApiTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * This file is part of the Swiftype App Search PHP Client package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Swiftype\AppSearch\Tests\Integration;
+
+/**
+ * Integration test for the Query Suggestions API.
+ *
+ * @package Swiftype\AppSearch\Test\Integration
+ *
+ * @author  Aurélien FOUCRET <aurelien.foucret@elastic.co>
+ */
+class QuerySuggestionApiTest extends AbstractEngineTestCase
+{
+    /**
+     * @var bool
+     */
+    protected static $importSampleDocs = true;
+
+    /**
+     * Run simple searches with optional pagination and check result returned.
+     *
+     * @param array $searchRequest The search request.
+     *
+     * @testWith ["cat", null, null]
+     *           ["gru", null, 4]
+     *           ["gru", ["title"], null]
+     *           ["gru", ["title"], 1]
+     */
+    public function testQuerySuggestion($queryText, $fields, $size)
+    {
+        $suggestions = $this->getDefaultClient()->querySuggestion($this->getDefaultEngineName(), $queryText, $fields, $size);
+
+        $this->assertNotEmpty($suggestions['meta']['request_id']);
+        $this->assertNotEmpty($suggestions['results']['documents']);
+
+        if ($size !== null) {
+            $this->assertLessThanOrEqual($size, count($suggestions['results']['documents']));
+        }
+
+        array_walk($suggestions['results']['documents'], function ($suggestion) {
+            $this->assertNotEmpty($suggestion['suggestion']);
+        });
+    }
+}
