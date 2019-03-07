427e1fd838e5d11758ed5045ae362c03359a3208
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-03-07 17:38:02 +0100

    Optimize search engine readiness detection.

--- Tests/Integration/AbstractEngineTestCase.php
+++ Tests/Integration/AbstractEngineTestCase.php
@@ -83,18 +83,27 @@ class AbstractEngineTestCase extends AbstractClientTestCase
 
         $indexingResponse = $client->indexDocuments($engineName, $documents);
 
-        if ($waitForSearchableDocs) {
-            do {
-                // We wait for the doc to be searchable before launching the test.
-                $searchResponse = $client->search($engineName, '');
-                $areDocsSynced = $searchResponse['meta']['page']['total_results'] == count($documents);
+            $isReady = false;
 
-                // We also wait for the schema to be synced.
-                $schema = $client->getSchema($engineName);
-                $isSchemaSynced = !empty($schema);
-                usleep(self::SYNC_RETRY_INTERVAL);
-            } while (!($areDocsSynced && $isSchemaSynced));
-        }
+            if ($waitForSearchableDocs) {
+                do {
+                    // We also wait for the schema to be synced.
+                    $schema = $client->getSchema($engineName);
+                    $isSchemaSynced = !empty($schema);
+
+                    if ($isSchemaSynced) {
+                        // We wait for the docs to be searchable before launching the test.
+                        $searchResponse = $client->search($engineName, '');
+                        $areDocsSynced = $searchResponse['meta']['page']['total_results'] == count($documents);
+
+                        $isReady = $isSchemaSynced && $areDocsSynced;
+                    }
+
+                    if (!$isReady) {
+                        usleep(self::SYNC_RETRY_INTERVAL);
+                    }
+                } while (false === $isReady);
+            }
 
         return $indexingResponse;
     }
