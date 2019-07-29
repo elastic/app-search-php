d5f89e8319cc82758cf1a1e397a0a3dcbe1dca5a
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2019-07-29 12:30:22 +0200

    Update endpoints headers.

--- Client.php
+++ Client.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This file is part of the Elastic OpenAPI PHP code generator.
+ * This file is part of the Elastic App Search PHP Client package.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
