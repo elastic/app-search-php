cd34d5e411c1d0647105f87d17c42e04f9ac89d1
miiimooo <michael.moritz@elastic.co>
2019-10-18 12:21:16 +0200

    Add some debug output

--- .circleci/create-admin-key.sh
+++ .circleci/create-admin-key.sh
@@ -11,10 +11,10 @@ function create_admin_key {
   local JSON=$(curl -s "${ES_URL}/.app-search-actastic-loco_moco_api_tokens/_search?q=authentication_token:private-*")
   # rewrite to an admin key
   local ADMIN_JSON=$(echo "$JSON"|jq -c '.hits.hits[0]._source'|sed s/private/admin/g)
-  curl -s -X PUT -H "Content-Type: application/json" "${ES_URL}/.app-search-actastic-loco_moco_api_tokens/_doc/phpintegrationtesting?pretty&refresh=true" \
+  curl -X PUT -H "Content-Type: application/json" "${ES_URL}/.app-search-actastic-loco_moco_api_tokens/_doc/phpintegrationtesting?pretty&refresh=true" \
     -d "$ADMIN_JSON"
 }
 
-#wait_for_as
+wait_for_as
 create_admin_key
 unset -f create_admin_key
