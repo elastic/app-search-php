2b72d474ebbae188dbb4f766b15844ce61d13f86
Aurélien FOUCRET <aurelien.foucret@elastic.co>
2020-05-14 11:08:50 +0200

    Update docker image to 7.7.0.

--- .circleci/retrieve-credentials.sh
+++ .circleci/retrieve-credentials.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 function wait_for_as {
   local AS_URL=${AS_URL:-"http://localhost:3002"}
-  curl --connect-timeout 5 --max-time 10 --retry 10 --retry-delay 0 --retry-max-time 120 --retry-connrefuse -s -o /dev/null ${AS_URL}/login
+  curl --connect-timeout 5 --max-time 10 --retry 10 --retry-delay 0 --retry-max-time 120 --retry-connrefuse -s -o /dev/null "${AS_URL}"
 }
 
 function load_api_keys {
+  local AS_USERNAME=${AS_USERNAME:-"enterprise_search"}
+  local AS_PASSWORD=${AS_PASSWORD:-"password"}
   local AS_URL=${AS_URL:-"http://localhost:3002"}
   local SEARCH_URL="${AS_URL}/as/credentials/collection?page%5Bcurrent%5D=1"
-  echo $(curl -uapp_search:password -s ${SEARCH_URL} | sed -E "s/.*(${1}-[[:alnum:]]{24}).*/\1/")
+  echo $(curl -u${AS_USERNAME}:${AS_PASSWORD} -s ${SEARCH_URL} | sed -E "s/.*(${1}-[[:alnum:]]{24}).*/\1/")
 }
 
 wait_for_as
